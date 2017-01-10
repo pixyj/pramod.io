ac8320da577ff16cbdcd42a594e804a54ce6de18
Pramod Lakshmanan <pramodliv1@gmail.com>
2017-01-11 01:06:28 +0530

    Copy changes to statically generated site

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
+# Assumes that django is running on 4444. Be careful about this!
 export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
-workon site
+workon pramod_io
 python manage.py create_static_site
 deactivate
 appcfg.py -A pixy-1310 -V v1 update helloworld/
